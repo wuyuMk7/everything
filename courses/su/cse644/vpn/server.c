dc2c637e3e245a5556ab00687fa70f3e611f40cc
wuyuMk7 <hymeldon@gmail.com>
2019-04-23 10:13:56 -0400

    feat(courses-su-cse644-vpn): add IP parser and async server sock
    Add IP parser to extract IP address from incoming packets.
    Server utilizes this to identify to which pipe
    the incoming packets should be sent.
    
    Add server socket to read set in order to listen it asynchronously.

--- courses/su/cse644/vpn/server.c
+++ courses/su/cse644/vpn/server.c
@@ -15,6 +15,7 @@
 #include <errno.h>
 
 #include "tun.h"
+#include "myheader.h"
 
 #define CLIENT_SIZE 255
 #define SERVER_PORT 12605
@@ -73,7 +74,7 @@ int main(int argc, char* argv[])
 
   SSL_METHOD *meth;
   SSL_CTX *ctx;
-  SSL *ssl;
+  //SSL *ssl;
 
   SSL_library_init();
   SSL_load_error_strings();
@@ -84,7 +85,7 @@ int main(int argc, char* argv[])
   SSL_CTX_set_verify(ctx, SSL_VERIFY_NONE, NULL);
   SSL_CTX_use_certificate_file(ctx, "./server_cert/vpnlabserver-hecrt.pem", SSL_FILETYPE_PEM);
   SSL_CTX_use_PrivateKey_file(ctx, "./server_cert/vpnlabserver-hekey.pem", SSL_FILETYPE_PEM);
-  ssl = SSL_new(ctx);
+  //ssl = SSL_new(ctx);
 
   for (int i = 0;i < CLIENT_SIZE; ++i) {
     client_socks[i] = -1;
@@ -92,13 +93,39 @@ int main(int argc, char* argv[])
   }
   client_socks[0] = server_sock;
   client_socks[1] = server_sock;
-  client_socks[2] = server_sock;
+  //client_socks[2] = server_sock;
 
   tunfd = createTunDevice("192.168.53.1", "255.255.255.0", 0);
-  send_tunfd = createTunDevice("192.168.54.2", "255.255.255.0", 0);
+  //send_tunfd = createTunDevice("192.168.54.2", "255.255.255.0", 0);
   server_sock = setupTCPServer();
+  //////////////////////////////////
+      printf("Main process\n");
+      struct ipheader *iph;
+      char main_recv_buffer[BUFFER_SIZE];
+      int main_recv_buffer_len = 0;
+      while (1) {
+	fd_set readFDSet;
 
-  while(1) {
+        FD_ZERO(&readFDSet);
+	FD_SET(server_sock, &readFDSet);
+        FD_SET(tunfd, &readFDSet);
+  	select(FD_SETSIZE, &readFDSet, NULL, NULL, NULL);
+
+ 	if (FD_ISSET(tunfd, &readFDSet)) {
+	  printf("Got a packet from TUN\n");	
+	  int main_recv_buffer_len = read(tunfd, main_recv_buffer, BUFFER_SIZE);
+	  printf("TUN:%d %s\n", main_recv_buffer_len, main_recv_buffer);
+	  iph = (struct ipheader*)(main_recv_buffer);
+	  //unsigned char *source_addr = (unsigned char*)&(iph->iph_sourceip.s_addr);
+	  unsigned char *dest_addr = (unsigned char*)&(iph->iph_destip.s_addr);
+	  /*
+	  printf("Source IP: %d.%d.%d.%d\n", source_addr[0], source_addr[1], source_addr[2], source_addr[3]);
+	  printf("Dest IP: %d\n", dest_addr[3]);
+	  */
+	  write(client_fds[dest_addr[3]][1], main_recv_buffer, main_recv_buffer_len);
+	}
+
+        if (FD_ISSET(server_sock, &readFDSet)) {
     int client_sock = accept(server_sock, (struct sockaddr*)&client_addr, &client_len);
     int cur = 0;
     for (; cur < CLIENT_SIZE && client_socks[cur] != -1;++cur) ;
@@ -113,6 +140,7 @@ int main(int argc, char* argv[])
       close(server_sock);
       close(client_fds[cur][1]);
 
+      SSL *ssl = SSL_new(ctx);
       SSL_set_fd(ssl, client_sock);
       if ((err = SSL_accept(ssl)) < 1) {
 	ERR_print_errors_fp(stderr);
@@ -194,26 +222,16 @@ int main(int argc, char* argv[])
       SSL_free(ssl);
       close(client_sock);
       return 0;
-    } else {
-      printf("Main process\n");
-      char main_recv_buffer[BUFFER_SIZE];
-      int main_recv_buffer_len = 0;
-      while (1) {
-	fd_set readFDSet;
+    } 
 
-        FD_ZERO(&readFDSet);
-        FD_SET(tunfd, &readFDSet);
-  	select(FD_SETSIZE, &readFDSet, NULL, NULL, NULL);
-
- 	if (FD_ISSET(tunfd, &readFDSet)) {
-	  printf("Got a packet from TUN\n");	
-	  int main_recv_buffer_len = read(tunfd, main_recv_buffer, BUFFER_SIZE);
-	  printf("TUN:%d %s\n", main_recv_buffer_len, main_recv_buffer);
-	  write(client_fds[3][1], main_recv_buffer, main_recv_buffer_len);
-	}
+ 	}
       }
-    }
-  }
+
+
+  ///////////////////////////////////
+
+  //while(1) {
+  //}
 
   return 0;
 }
