5c3f49524aed1960223c4e89063e0c5ccb8df3fe
Allen He <hymeldon@gmail.com>
2019-05-10 11:57:34 -0400

    Update config.py
    Remove email address

--- courses/su/cse775/final/raspberrypi/collector/config.py
+++ courses/su/cse775/final/raspberrypi/collector/config.py
@@ -5,7 +5,7 @@ import os
 
 FIREBASE_CREDS_JSON = os.path.dirname(os.path.realpath(__file__)) + '/FB_AUTH_KEY.json'
 FIREBASE_INTERVAL = 60
-FIREBASE_COLLECTION_NAME='xzhan145@gmail.com'
+FIREBASE_COLLECTION_NAME=''
 FIREBASE_DOCUMENT_NAME='sitting-signal'
 FIREBASE_SITTING_SUBCOLLECTION_NAME='raw_data'
 
